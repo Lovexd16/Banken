944a3390619e2eeeb0e288778828547c0be7c0cc
Love Rumar Karlquist <lovebandy@hotmail.se>
2023-09-21 12:47:29 +0200

    rättade till en kommentar

--- Inlämningsuppgift Banken/src/App.java
+++ Inlämningsuppgift Banken/src/App.java
@@ -139,7 +139,7 @@ public class App {
         account.withdraw(withdrawAmount);
     }
 
-    //För case 3. Kollar att man inte tar ut mer än vad saldot har, och att man tar ut mer än 0kr. Skriver även ut hur mycket man tog ut.
+    //För case 3. Uppdaterar saldot och berättar hur mycket man tagit ut. Kollar att man inte tar ut mer än vad saldot har, och att man tar ut mer än 0kr. Skriver även ut hur mycket man tog ut.
     public void withdraw(double withdrawAmount) {
 
             if (this.balance >= withdrawAmount && withdrawAmount > 0) {
